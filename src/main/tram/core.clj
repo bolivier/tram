4cbcd424e5c03ca0478e963e1252a6f77b646c9a
Brandon Olivier <brandon@brandonolivier.com>
2025-06-19 22:02:16 -0500

    Add core file

---
+++ src/main/tram/core.clj
@@ -0,0 +1,48 @@
+(ns tram.core
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]))
+
+(defn generate-config
+  "Creates default configuration file contents for tram."
+  [project-name]
+  {:project/name         project-name
+   :database/test        {:store :database
+                          :migration-dir "migrations/"
+                          :migration-table-name "migrations"
+                          :db {:dbtype "postgresql"
+                               :dbname (str project-name "_test")}}
+   :database/development {:store :database
+                          :migration-dir "migrations/"
+                          :migration-table-name "migrations"
+                          :db {:dbtype "postgresql"
+                               :dbname (str project-name "_development")}}
+   :database/prod        {:store :database
+                          :migration-dir "migrations/"
+                          :migration-table-name "migrations"
+                          :db {:dbtype "postgresql"
+                               :dbname (str project-name "_production")}}})
+
+(defn new [& args]
+  (prn "Running as main with args:" args))
+
+(defn get-zprint-config []
+  (with-open [r (java.io.PushbackReader. (io/reader (io/file ".zprint.edn")))]
+    (edn/read r)))
+
+(defn get-env []
+  (System/getenv "TRAM_ENV"))
+
+(defn read-config []
+  (with-open [r (java.io.PushbackReader. (io/reader (io/file "tram.edn")))]
+    (edn/read r)))
+
+(defn get-migration-config [env]
+  (let [config (read-config)
+        db-key (keyword "database" env)]
+    (get config db-key)))
+
+(defn get-database-config [env]
+  (get (get-migration-config env) :db))
+
+(defn get-database-name [env]
+  (get (get-database-config env) :dbname))
