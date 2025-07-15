c33567fe678e5a4843760178073cebf5e516acae
Brandon Olivier <brandon@brandonolivier.com>
2025-07-15 10:41:07 -0500

    Update testing

--- src/test-project/sample_app/views/sign_in_views.clj
+++
@@ -1,7 +0,0 @@
-(ns sample-app.views.sign-in-views)
-
-(defn show-user [_context]
-  "show user page")
-
-(defn sign-in [_ctx]
-  "sign in here")
---
+++ src/test/tram/rendering/template_renderer_test.clj
@@ -0,0 +1,31 @@
+(ns tram.rendering.template-renderer-test
+  (:require
+    [expectations.clojure.test :as e]
+    [sample-app.handlers.authentication-handlers :refer [routes sample-router]]
+    [sample-app.views.authentication-views :as views]
+    [tram.rendering.template-renderer :as sut]))
+
+(e/defexpect renderer
+  (e/expecting "nil template rendering"
+               (let [request {:uri "/sign-in"
+                              :request-method :get
+                              :reitit.core/router sample-router}
+                     ctx     (sut/render {:request  request
+                                          :response {}})]
+                 (e/expect (views/sign-in nil) (:body (:response ctx)))))
+  (e/expecting
+    "keyword template rendering"
+    (let [match   (-> sample-router
+                      (reitit.core/match-by-name :route/forgot-password))
+          handler (-> sample-router
+                      (reitit.core/match-by-name :route/forgot-password)
+                      :data
+                      :get
+                      :handler)
+          request {:uri "/forgot-password"
+                   :request-method :get
+                   :reitit.core/match match
+                   :reitit.core/router sample-router}
+          ctx     (sut/render {:request  request
+                               :response (handler request)})]
+      (e/expect (views/forgot-password nil) (:body (:response ctx))))))
