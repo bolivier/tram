07d5b4349adcae3e26026029cda5939dcd0c8114
Brandon Olivier <brandon@brandonolivier.com>
2025-07-19 13:31:07 -0500

    Wrap new command with process defaults

--- src/cli/tram_cli/generator/new.clj
+++ src/cli/tram_cli/generator/new.clj
@@ -22,46 +22,53 @@
   (validate-project-name! project-name)
   (let [project-root  (io/file called-from-dir project-name)
         template-root (io/file "resources/tram/templates/starter-template")]
-    (try
-      (fs/create-dir project-root)
-      (catch Exception _
-        (println (str "Directory "
-                      project-root
-                      " already exists.  Please remove it and try again."))
-        (System/exit 1)))
-    (println "Copying files")
-    (doseq [src  (->> (file-seq template-root)
-                      (filter #(.isFile %)))
-            :let [relative (->
-                             (.getPath src)
-                             (str/replace-first
-                               #".*?resources/tram/templates/starter-template/"
-                               "")
-                             (str/replace "sample_app" (ns->path project-name)))
-                  dest     (io/file project-root relative)]]
-      (io/make-parents dest)
-      (spit dest
-            (-> src
-                slurp
-                (str/replace "sample_app" (->snake_case project-name))
-                (str/replace "sample-app" project-name))))
-    (doseq [db-script ["bin/db-init" "bin/db-start"]]
-      (fs/copy (io/file template-root db-script)
-               (io/file project-root db-script)
-               {:copy-attributes  true
-                :replace-existing true})
-      (spit (io/file project-root db-script)
-            (-> (io/file project-root db-script)
-                slurp
-                (str/replace "sample_app" (->snake_case project-name))
-                (str/replace "sample-app" project-name))))
-    (println "Initializing a git repo.")
-    (p/shell {:dir project-root} "git init")
-    (p/shell {:dir project-root} "git add .")
-    (p/shell {:dir project-root} "git commit -m 'Initial commit'")
-    (println "Installing development tools with mise.")
-    (p/shell {:dir project-root} "mise install")
-    (println "Creating development and test databases")
-    (p/shell {:dir project-root} "mise trust")
-    (p/shell {:dir project-root} "mise exec -- ./bin/db-init")
-    (p/shell {:dir project-root} "tram db:migrate")))
+    (binding [p/*defaults* (assoc p/*defaults*
+                             :dir      project-root
+                             :continue true)]
+      (try
+        (fs/create-dir project-root)
+        (catch Exception _
+          (println (str "Directory "
+                        project-root
+                        " already exists.  Please remove it and try again."))
+          (System/exit 1)))
+      (println "Copying files")
+      (doseq [src  (->> (file-seq template-root)
+                        (filter #(.isFile %)))
+              :let [relative
+                    (-> (.getPath src)
+                        (str/replace-first
+                          #".*?resources/tram/templates/starter-template/"
+                          "")
+                        (str/replace "sample_app" (ns->path project-name)))
+
+                    dest (io/file project-root relative)]]
+        (io/make-parents dest)
+        (spit dest
+              (-> src
+                  slurp
+                  (str/replace "sample_app" (->snake_case project-name))
+                  (str/replace "sample-app" project-name))))
+      (doseq [db-script ["bin/db-init" "bin/db-start"]]
+        (fs/copy (io/file template-root db-script)
+                 (io/file project-root db-script)
+                 {:copy-attributes  true
+                  :replace-existing true})
+        (spit (io/file project-root db-script)
+              (-> (io/file project-root db-script)
+                  slurp
+                  (str/replace "sample_app" (->snake_case project-name))
+                  (str/replace "sample-app" project-name))))
+      (println "Initializing a git repo.")
+      (p/shell "git init")
+      (p/shell "git add .")
+      (p/shell "git commit -m 'Initial commit'")
+      (println "Installing development tools with mise.")
+      (p/shell "mise install")
+      (p/shell "mise trust")
+      (println "Creating development and test databases")
+      (println "Next Steps: ")
+      (println
+        "  To initialize your database, run:  $ mise exec -- ./bin/db-init")
+      (println "  To migrate your new database, run:  $ tram db:migrate")
+      (println "  Start your server with $ tram dev"))))
