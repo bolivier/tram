f45d2370f6d4729ca37cd8be7785280d938965cc
Brandon Olivier <brandon@brandonolivier.com>
2025-07-19 14:43:17 -0500

    Configure all taskrunning through bb

--- src/cli/tram_cli/entry.clj
+++ src/cli/tram_cli/entry.clj
@@ -9,38 +9,32 @@
             [tram-cli.nrepl-client :as client]
             [zprint.core :refer [zprint-file-str]]))
 
+(alter-var-root #'p/*defaults* #(assoc % :continue true))
+
 (defn -main [& args]
   (let [cmd (first args)]
     (case cmd
-      "daemon:start" (start-daemon)
-      "daemon:kill" (kill-daemon)
-      "daemon:restart" (restart-daemon)
-      "daemon:status" (if (accepting?)
-                        (println "Running")
-                        (println "Stopped"))
       "new" (render-new-project-template (first (rest args)))
       "test:watch"
       (if (fs/exists? "bin/kaocha")
         (do (println "Watching tests with bin/kaocha")
-            (p/shell {:continue true}
-                     "bin/kaocha --watch"))
+            (p/shell "bin/kaocha --watch"))
         (do
           (println
             "bin/kaocha not found, invoking clojure command `clojure -X:test:watch`")
-          (p/shell {:continue true}
-                   "clojure -X:test:watch")))
+          (p/shell "clojure -X:test:watch")))
 
       "test"
       (if (fs/exists? "bin/kaocha")
         (do (println "Running tests with bin/kaocha")
-            (p/shell {:continue true}
-                     "bin/kaocha"))
+            (p/shell "bin/kaocha"))
         (do
           (println
             "bin/kaocha not found, invoking clojure command `clojure -X:test`")
-          (p/shell {:continue true}
-                   "clojure -X:test")))
+          (p/shell "clojure -X:test")))
 
-      (client/send {:op   :tram/op
-                    :cmd  cmd
-                    :args (rest args)}))))
+      "db:migrate" (println "Migrating database")
+      (println "Unknown command" cmd)
+      #_(client/send {:op   :tram/op
+                      :cmd  cmd
+                      :args (rest args)}))))
