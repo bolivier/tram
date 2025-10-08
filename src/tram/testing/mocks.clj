3c21013a865cc6103790e648cdead44dd6569078
Brandon Olivier <brandon@brandonolivier.com>
2025-10-08 09:20:58 -0500

    feat: add with-stub

--- src/tram/testing/mocks.clj
+++ src/tram/testing/mocks.clj
@@ -33,23 +33,50 @@
      ~@body))
 
 (defmacro with-stub
-  [[func opts] & body]
-  (let [return-value (:returns opts)
-        return-fn    (:return-fn opts)
-        args         (gensym)]
-    `(let [return-value# ~return-value
-           return-fn#    ~return-fn]
-       (with-redefs [*calls* (if @*calls*
-                               *calls*
-                               (atom []))
-                     ~func   (fn [& ~args]
-                               (swap! *calls* conj
-                                 {:func ~func
-                                  :args ~args})
-                               (cond
-                                 ~(some? return-value) return-value#
-                                 ~(some? return-fn)    (return-fn# ~args)
-                                 :else                 nil))]
+  [bindings & body]
+  (let [stubs (partition 2 bindings)
+
+        ;; Process each stub into normalized options with generated syms
+        stub-configs (map (fn [[calls-sym fn-or-opts]]
+                            (let [calls-atom-sym (gensym "calls-val-")
+                                  return-sym (gensym "return-value-")
+                                  impl-sym (gensym "impl-")]
+                              (if (map? fn-or-opts)
+                                (assoc fn-or-opts
+                                       :calls-sym calls-sym
+                                       :calls-atom-sym calls-atom-sym
+                                       :return-sym return-sym
+                                       :impl-sym impl-sym)
+                                {:fn fn-or-opts
+                                 :calls-sym calls-sym
+                                 :calls-atom-sym calls-atom-sym
+                                 :return-sym return-sym
+                                 :impl-sym impl-sym})))
+                          stubs)
+
+        ;; Generate let bindings for calls atoms and return/impl values
+        let-bindings (mapcat (fn [opts]
+                               [(:return-sym opts) (:returns opts)
+                                (:impl-sym opts) (:impl opts)
+                                (:calls-atom-sym opts) `(atom [])
+                                (or (:calls-sym opts) (gensym)) (:calls-atom-sym opts)])
+                             stub-configs)
+
+        ;; Generate with-redefs pairs
+        redef-pairs (mapcat (fn [opts]
+                              [(:fn opts)
+                               `(fn [& args#]
+                                  (swap! ~(:calls-atom-sym opts) conj
+                                         {:func ~(:fn opts)
+                                          :args args#})
+                                  (cond
+                                    (some? ~(:return-sym opts)) ~(:return-sym opts)
+                                    (some? ~(:impl-sym opts)) (apply ~(:impl-sym opts) args#)
+                                    :else nil))])
+                            stub-configs)]
+
+    `(let [~@let-bindings]
+       (with-redefs [~@redef-pairs]
          ~@body))))
 
 (defmacro with-temp-ns
@@ -59,4 +86,4 @@
          (require '[clojure.core :refer :all])
          ~@(map (fn [form]
                   `(eval '~form))
-             body))))
+                body))))
