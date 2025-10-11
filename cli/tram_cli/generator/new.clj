3f7db0a009c19f67d0210b26cfad913c41865fa9
Brandon Olivier <brandon@brandonolivier.com>
2025-10-11 15:14:10 -0500

    feat: help menu in the CLI

--- cli/tram_cli/generator/new.clj
+++ cli/tram_cli/generator/new.clj
@@ -5,43 +5,43 @@
             [clojure.java.io :as io]
             [clojure.string :as str]))
 
-(def called-from-dir
-  (io/file (System/getenv "TRAM_CLI_CALLED_FROM")))
+(def called-from-dir (io/file (System/getenv "TRAM_CLI_CALLED_FROM")))
 
-(defn ns->path [ns]
+(defn ns->path
+  [ns]
   (-> ns
       (str/replace "-" "_")
       (str/replace "." "/")))
 
-(defn validate-project-name! [n]
+(defn validate-project-name!
+  [n]
   (when-not (= (->kebab-case n) n)
     (println "Error: project name must be kebab-case.")
     (System/exit 1)))
 
-(defn render-new-project-template [project-name]
+(defn render-new-project-template
+  [project-name]
+  (prn "calling real")
+  (throw (Exception. "butt"))
   (validate-project-name! project-name)
-  (let [project-root  (io/file called-from-dir project-name)
+  (let [project-root (io/file called-from-dir project-name)
         template-root (io/file "starter-template")]
     (binding [p/*defaults* (assoc p/*defaults*
-                             :dir      project-root
+                             :dir project-root
                              :continue true)]
-      (try
-        (fs/create-dir project-root)
-        (catch Exception _
-          (println (str "Directory "
-                        project-root
-                        " already exists.  Please remove it and try again."))
-          (System/exit 1)))
+      (try (fs/create-dir project-root)
+           (catch Exception _
+             (println (str "Directory "
+                           project-root
+                           " already exists.  Please remove it and try again."))
+             (System/exit 1)))
       (println "Copying files")
-      (doseq [src  (->> (file-seq template-root)
-                        (filter #(.isFile %)))
-              :let [relative
-                    (-> (.getPath src)
-                        (str/replace-first
-                          #".*starter-template/"
-                          "")
-                        (str/replace "sample_app" (ns->path project-name)))
-
+      (doseq [src (->> (file-seq template-root)
+                       (filter #(.isFile %)))
+              :let [relative (-> (.getPath src)
+                                 (str/replace-first #".*starter-template/" "")
+                                 (str/replace "sample_app"
+                                              (ns->path project-name)))
                     dest (io/file project-root relative)]]
         (io/make-parents dest)
         (spit dest
@@ -52,8 +52,7 @@
       (doseq [db-script ["bin/db-init"]]
         (fs/copy (io/file template-root db-script)
                  (io/file project-root db-script)
-                 {:copy-attributes  true
-                  :replace-existing true})
+                 {:copy-attributes true, :replace-existing true})
         (spit (io/file project-root db-script)
               (-> (io/file project-root db-script)
                   slurp
