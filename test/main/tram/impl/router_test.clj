8d1ba9ac7ffeafd2cb2116902c435eafeb942f2d
Brandon Olivier <brandon@brandonolivier.com>
2025-10-24 11:51:12 -0500

    Testing cleanup

--- test/main/tram/impl/router_test.clj
+++ test/main/tram/impl/router_test.clj
@@ -1,5 +1,5 @@
-(ns tram.impl.router_test
-  (:require [clojure.test :refer [deftest is]]
+(ns tram.impl.router-test
+  (:require [clojure.test :refer [deftest is testing]]
             [matcher-combinators.test]
             [test-app.views.authentication-views :as views]
             [tram.impl.router :as sut]))
@@ -10,8 +10,18 @@
     (is (match? {:layouts [layout-fn]} ctx)
         "Layout interceptor did not add layout-fn to :layouts key.")))
 
-(deftest coerce-route-entries-to-specs-test
-  (is (match? {:interceptors [{:name  :tram.impl.router/layout-interceptor
-                               :enter fn?}]
-               :layout       views/layout}
-              (sut/coerce-route-entries-to-specs {:layout views/layout}))))
+(deftest coerce-layout-test
+  ;; Since these operate on macro args, they will be called in the handlers
+  ;; ns
+  (binding [*ns* (the-ns 'test-app.handlers.authentication-handlers)]
+    (testing "Using a function/symbol"
+      (is (match?
+            {:interceptors ['(tram.impl.router/layout-interceptor views/layout)]
+             :layout       'views/layout}
+            (sut/coerce-route-entries-to-specs '{:layout views/layout}))))
+    (testing "Using a keyword"
+      (is (match?
+            {:interceptors [`(tram.impl.router/layout-interceptor views/layout)]
+             :layout       :view/layout}
+            (binding [*ns* (the-ns 'test-app.handlers.authentication-handlers)]
+              (sut/coerce-route-entries-to-specs '{:layout :view/layout})))))))
