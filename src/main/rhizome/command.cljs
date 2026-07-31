8993945cf45bc33c24c7b2f31e2b89e880ef6a19
Brandon Olivier <brandon@brandonolivier.com>
2026-07-30 21:46:34 -0500

    refactor: delete the old rhizome client runtime

--- src/main/rhizome/command.cljs
+++
@@ -1,8 +0,0 @@
-(ns rhizome.command)
-
-(defmulti execute
-  "Execute a rhizome command.  Takes 3 args, dispatches on `:op` in `cmd`.
-
-  - `el` is the dom node the event is defined on 
-  - `cmd` is the command being executed"
-  (fn [el cmd] (:op cmd)))
