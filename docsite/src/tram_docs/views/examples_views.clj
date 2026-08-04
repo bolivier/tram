164a0a2457ed256c07ec23f39889b16bec3147a9
Brandon Olivier <brandon@brandonolivier.com>
2026-08-04 07:04:57 -0500

    feat: card ui around each example demo

--- docsite/src/tram_docs/views/examples_views.clj
+++ docsite/src/tram_docs/views/examples_views.clj
@@ -2,6 +2,16 @@
   (:require [rhizome.core :as rz]
             [tram.routes :as tr]))
 
+(defn demo-card
+  "Chrome that marks the running demo off from the prose around it.
+
+  Goes inside an example's morph target, so a swap replaces the demo and
+  leaves the card alone."
+  [& children]
+  [:div.demo-card
+   [:p.demo-card-label "Demo"]
+   [:div.demo-card-body children]])
+
 (defn global-button [count]
   [:button#global-counter.btn.primary {::rz/click
                                        {:do :http/post
@@ -17,9 +27,7 @@
    (str "Increment User: " count)])
 
 (defn counter-example [{:keys [global-count user-count]}]
-  [:div
-   [global-button global-count]
-   [user-button user-count]])
+  [demo-card [global-button global-count] [user-button user-count]])
 
 (defn layout [children]
   [:main#main
@@ -80,49 +88,52 @@
 
 (defn click-to-load-example [{:keys [queue next-page]}]
   [:div#click-to-load-example
-   [:table.table#visa-queue
-    [:thead
-     [:tr
-      [:th "Name"]
-      [:th "Nationality"]
-      [:th "Papers"]]]
-    [:tbody
-     (for [refugee queue]
-       [:tr.person-row
-        [:td (:name refugee)]
-        [:td (:nationality refugee)]
-        [:td (:papers refugee)]])]]
-   (if next-page
-     [:button.btn.primary {::rz/click {:do       :http/get
-                                       :http/url (tr/make-route
-                                                   :route/examples.click-to-load
-                                                   {:tram.routes/query
-                                                    {:loaded next-page}})}}
-      "Load 3 more"]
-     [:p "Everyone is on the list. Nobody is on the plane."])])
+   [demo-card
+    [:table.table#visa-queue
+     [:thead
+      [:tr
+       [:th "Name"]
+       [:th "Nationality"]
+       [:th "Papers"]]]
+     [:tbody
+      (for [refugee queue]
+        [:tr.person-row
+         [:td (:name refugee)]
+         [:td (:nationality refugee)]
+         [:td (:papers refugee)]])]]
+    (if next-page
+      [:button.btn.primary {::rz/click {:do :http/get
+                                        :http/url
+                                        (tr/make-route
+                                          :route/examples.click-to-load
+                                          {:tram.routes/query {:loaded
+                                                               next-page}})}}
+       "Load 3 more"]
+      [:p "Everyone is on the list. Nobody is on the plane."])]])
 
 (defn click-to-edit-example [{:keys [patron editing?]}]
   [:div#click-to-edit-example
-   (if editing?
-     [:form#patron-form {::rz/submit {:do       :http/patch
-                                      :http/url :route/examples.click-to-edit}}
-      [:input.input {:name  :name
-                     :value (:name patron)}]
-      [:input.input {:name  :role
-                     :value (:role patron)}]
-      [:div.flex.gap-2
-       [:button.btn {:type      :button
-                     ::rz/click {:do       :http/get
-                                 :http/url :route/examples.click-to-edit}}
-        "Cancel"]
-       [:button.btn.primary {:type :submit}
-        "Save"]]]
-     [:div
-      [:h3 (:name patron)]
-      [:p (:role patron)]
-      [:button.btn {::rz/click {:do       :http/get
-                                :http/url :route/examples.click-to-edit.form}}
-       "Edit"]])])
+   [demo-card
+    (if editing?
+      [:form#patron-form {::rz/submit {:do       :http/patch
+                                       :http/url :route/examples.click-to-edit}}
+       [:input.input {:name  :name
+                      :value (:name patron)}]
+       [:input.input {:name  :role
+                      :value (:role patron)}]
+       [:div.flex.gap-2
+        [:button.btn {:type      :button
+                      ::rz/click {:do       :http/get
+                                  :http/url :route/examples.click-to-edit}}
+         "Cancel"]
+        [:button.btn.primary {:type :submit}
+         "Save"]]]
+      [:div
+       [:h3 (:name patron)]
+       [:p (:role patron)]
+       [:button.btn {::rz/click {:do       :http/get
+                                 :http/url :route/examples.click-to-edit.form}}
+        "Edit"]])]])
 
 (defn search-results [{:keys [crew]}]
   [:tbody#search-results
@@ -138,38 +149,40 @@
 
 (defn active-search-example [locals]
   [:div#active-search-example
-   [:form#crew-search-form
-    [:input.input {:autocomplete "off"
-                   :name         :query
-                   :placeholder  "Search the squadron"
-                   ::rz/input    {:do :http/post
-                                  :http/url
-                                  :route/examples.active-search.results}}]]
-   [:table.table#squadron-table
-    [:thead
-     [:tr
-      [:th "Name"]
-      [:th "Rank"]
-      [:th "Duty"]]]
-    [search-results locals]]])
+   [demo-card
+    [:form#crew-search-form
+     [:input.input {:autocomplete "off"
+                    :name         :query
+                    :placeholder  "Search the squadron"
+                    ::rz/input    {:do :http/post
+                                   :http/url
+                                   :route/examples.active-search.results}}]]
+    [:table.table#squadron-table
+     [:thead
+      [:tr
+       [:th "Name"]
+       [:th "Rank"]
+       [:th "Duty"]]]
+     [search-results locals]]]])
 
 (defn edit-row-example [locals]
   (let [edit-id (:editable locals)]
     [:div#edit-row-example
-     (when edit-id
-       [:form#edit-person-form
-        {::rz/submit {:do       :http/patch
-                      :http/url (tr/make-route :route/examples.do-edit-row
-                                               {:id edit-id})}}])
-     [:table.table#people-table
-      [:thead
-       [:tr
-        [:th "Name"]
-        [:th "Email"]
-        [:th "Actions"]]]
-      (for [person (:people locals)
-            :let   [editing? (= edit-id (:id person))]]
-        (if editing?
-          [active-edit-row person]
-          [row {:disabled (some? edit-id)}
-           person]))]]))
+     [demo-card
+      (when edit-id
+        [:form#edit-person-form
+         {::rz/submit {:do       :http/patch
+                       :http/url (tr/make-route :route/examples.do-edit-row
+                                                {:id edit-id})}}])
+      [:table.table#people-table
+       [:thead
+        [:tr
+         [:th "Name"]
+         [:th "Email"]
+         [:th "Actions"]]]
+       (for [person (:people locals)
+             :let   [editing? (= edit-id (:id person))]]
+         (if editing?
+           [active-edit-row person]
+           [row {:disabled (some? edit-id)}
+            person]))]]]))
