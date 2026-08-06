bb49afca3bc99c788354cc71eace006736dd9b5d
Brandon Olivier <brandon@brandonolivier.com>
2026-08-06 13:18:35 -0700

    refactor: bugfixes

--- docsite/src/tram_docs/handlers/example_counter_handlers.clj
+++ docsite/src/tram_docs/handlers/example_counter_handlers.clj
@@ -4,34 +4,35 @@
             [tram.routes :as tr]
             [tram.sse :as sse]
             [tram.sse.async :as sse.async]))
-
-(defonce global
-  (atom 420))
-(defonce user
-  (atom 67))
+(defonce state
+  (atom {:global-count 420
+         :user-count   67}))
 
 (defn click-global [req]
-  (swap! global inc)
+  (swap! state update
+    :global-count
+    inc)
   {:status 204
    :body   nil})
 
 (defn click-user [req]
-  (swap! user inc)
-  (swap! global inc)
+  (swap! state (fn [state]
+                 (-> state
+                     (update :user-count inc)
+                     (update :global-count inc))))
   {:status 204
    :body   nil})
 
 (defn counter-example [req]
   {:status 200
-   :locals {:user-count   @user
-            :global-count @global}})
+   :locals @state})
 
 (defn- send-current!
   "The card rendered before its stream opened. Sending both buttons up front
   closes the window where another visitor's click landed in between."
   [ch]
-  (and (async/>!! ch (sse/morph [v/global-button @global]))
-       (async/>!! ch (sse/morph [v/user-button @user]))))
+  (and (async/>!! ch (sse/morph [v/global-button (:global-count @state)]))
+       (async/>!! ch (sse/morph [v/user-button (:user-count @state)]))))
 
 (defn- follow-counters!
   "Morphs a button every time its counter changes, until the client leaves.
@@ -40,22 +41,19 @@
   buffer rather than blocking a click. `>!!` returns false once the drain loop
   has closed `ch`, which is how a closed connection ends this loop."
   [ch]
-  (let [changes (async/chan (async/sliding-buffer 8))
-        follow! (fn [counter button]
-                  (add-watch counter
-                             changes
-                             (fn [_ _ _ n] (async/put! changes [button n]))))]
-    (follow! global v/global-button)
-    (follow! user v/user-button)
+  (let [changes (async/chan (async/sliding-buffer 8))]
+    (add-watch state
+               changes
+               (fn [_ _ _ n]
+                 (async/put! changes [v/global-button (:global-count n)])
+                 (async/put! changes [v/user-button (:user-count n)])))
     (try
       (loop []
         (when-let [content (async/<!! changes)]
           (when (async/>!! ch
                            (sse/morph content))
             (recur))))
-      (finally (remove-watch global changes)
-               (remove-watch user changes)
-               (async/close! changes)))))
+      (finally (remove-watch state changes) (async/close! changes)))))
 
 (defn counter-stream [req]
   {:status 200
--- docsite/tasks/dev/tailwind.clj
+++ docsite/tasks/dev/tailwind.clj
@@ -2,11 +2,11 @@
   (:require [babashka.process :as p]))
 
 (defn -main [& _]
-  @(p/process {:out :inherit
-               :err :inherit
-               :dir "resources/tailwindcss"}
-              "npm i")
-  @(p/process {:out :inherit
-               :err :inherit
-               :dir "resources/tailwindcss"}
-              "npm run dev"))
+  #_@(p/process {:out :inherit
+                 :err :inherit
+                 :dir "resources/tailwindcss"}
+                "npm i")
+  #_@(p/process {:out :inherit
+                 :err :inherit
+                 :dir "resources/tailwindcss"}
+                "npm run dev"))
