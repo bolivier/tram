c028514bbc65dfb20926680191dae46f6cdd5969
Brandon Olivier <brandon@brandonolivier.com>
2026-08-27 15:24:00 -0500

    feat: use tram.executor in the docsite

--- docsite/src/tram_docs/server.clj
+++ docsite/src/tram_docs/server.clj
@@ -2,17 +2,17 @@
   (:require [integrant.core :as ig]
             [org.httpkit.server :as hk-server]
             [reitit.http :as http]
-            [reitit.interceptor.sieppari :as sieppari]
             [reitit.ring :as ring]
             [tram-docs.config :as sys]
-            [tram-docs.routes]))
+            [tram-docs.routes]
+            [tram.executor :as executor]))
 
 (defmethod ig/init-key ::sys/app
   [_ {:keys [router]}]
   (http/ring-handler router
                      (ring/routes (ring/create-default-handler)
                                   (ring/redirect-trailing-slash-handler))
-                     {:executor sieppari/executor}))
+                     {:executor executor/executor}))
 
 (defmethod ig/init-key ::sys/server
   [_ {:keys [app port]}]
